a8a5fcedb1fa68d8b3bf90236e61f04ef135b382
marcnt <44109710+marcnt@users.noreply.github.com>
2019-06-19 18:12:30 +0800

    Fix array declaration

--- Bus.java
+++ Bus.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Bus
 {
   private static final String[] ROUTES = new String[] { "MNL to STC", "STC to MNL" };
-  private static final String[] STOPS = new String[]
+  private static final String[][] STOPS = new String[]
   {
     new String[] {  },
     new String[] {  }
