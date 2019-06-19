234ffb7c5cf9667836924c88e5ab94258fd9e526
Kirsten S <daniellesison19@gmail.com>
2019-06-19 18:27:33 +0800

    Added load method

--- Bus.java
+++ Bus.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class Bus
 {
+  // Attributes
   private static final String[] ROUTES = new String[] { "MNL to STC", "STC to MNL" };
-  private static final String[][] STOPS = new String[]
+  private static final String[][] STOPS = new String[][]
   {
     new String[] {  },
     new String[] {  }
@@ -14,5 +15,21 @@ public class Bus
   private ArrayList<Passenger> passengers;
   private int route;
   private int stop;
+
+  public Bus(String plateNumber, int capacity, int route)
+  {
+    this.plateNumber = plateNumber;
+    this.capacity = capacity;
+    this.route = route
+  }
+
+  public void load(Passenger person)
+  {
+    if (passengers.size() < capacity)
+      passengers.add(person);
+    else
+      System.out.println("Bus " + plateNumber + " is already full.");
+  }
+
 
 }
