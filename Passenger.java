47ebc61d6604b7e3a76d81f7bf36a64fd58ed195
Kirsten S <daniellesison19@gmail.com>
2019-06-19 17:28:13 +0800

    Added Passenger Constructor

--- Passenger.java
+++ Passenger.java
@@ -1,5 +1,15 @@
 public class Passenger {
 // Attributes
 private final String name;
-private final
+private final int priorityLvl;
+private final int route;
+private final int stop;
+
+public Passenger (String name, int priorityLvl, int route, int stop){
+  this.name = name;
+  this.priorityLvl = priorityLvl;
+  this.route = route;
+  this.stop = stop;
+}
+
 }
