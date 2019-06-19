ed124b1b1210e4cdcefb3657cfd4b0ceff798e15
marcnt <44109710+marcnt@users.noreply.github.com>
2019-06-19 18:08:36 +0800

    Add fields to Bus class

--- Bus.java
+++ Bus.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class Bus
 {
-  private ;
+  private static final String[] ROUTES = new String[] { "MNL to STC", "STC to MNL" };
+  private static final String[] STOPS = new String[]
+  {
+    new String[] {  },
+    new String[] {  }
+  };
+
+  private final String plateNumber;
+  private final int capacity;
+  private ArrayList<Passenger> passengers;
+  private int route;
+  private int stop;
+
 }
