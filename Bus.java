a4ff4ce0e9299d9a7f584bcff236f0225c3d540c
marcnt <44109710+marcnt@users.noreply.github.com>
2019-06-19 18:40:52 +0800

    Add sorting of passengers inside bus

--- Bus.java
+++ Bus.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Bus
 {
+  public class PassengerComparator implements Comparator<Passenger>
+  {
+    @Override
+    public int compare(Passenger a, Passenger b)
+    {
+      return Math.signum(b.getPriority() - a.getPriority());
+    }
+  }
+
   // Attributes
   private static final String[] ROUTES = new String[] { "MNL to STC", "STC to MNL" };
   private static final String[][] STOPS = new String[][]
@@ -26,7 +37,10 @@ public class Bus
   public void load(Passenger person)
   {
     if (passengers.size() < capacity)
+    {
       passengers.add(person);
+      Collections.sort(passengers, new PassengerComparator());
+    }
     else
       System.out.println("Bus " + plateNumber + " is already full.");
   }
